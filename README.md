523283590ead8b18f6a90bdf3bf5bd034314b841
Liz Borchardt <zardra@gmail.com>
2013-11-19 16:22:32 -0800

    added conversion function

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Transformation steps
 1. Open text file.
 2. Read it in by line (.readlines()).
 3. Reverse list order because instructions read from bottom of chart up.
-4. How to differentiate between charts with patterning on all rows and those that knit plain on WS?
+4. How to differentiate between charts with patterning on all rows and those that knit plain on WS?  Maybe a radio button the user can select before sending through conversion?
 5. Create empty list? Or change the list in place? In place would make keeping the rows separate easier.
 6. Either before running the list through the symbol key or after (which would be better?), need to reverse the order of the patterned WS rows (i.e., in relation to #4).
 7. Send each row through a function that will consolidate multiple instructions (i.e., "knit, knit, knit" vs. "knit 3 times").
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 def reverse_even_rows(text): 
-    """if chart contains both patterned right-side (RS) and wrong-side (WS) rows,
-    this will reverse the direction of the WS rows so they read in the correct direction."""
+    """if chart contains both patterned right-side (RS) and wrong-side (WS)
+    rows, this will reverse the direction of the WS rows so they read in
+    the correct direction."""
+
     i = 0
     l = []
     
@@ -13,35 +15,45 @@ def reverse_even_rows(text):
     
     return text
 
+def change_to_symbols(text):
+    """Takes each item from the text file and changes the numbers to the written instructions"""
 
-def transform_multiple(words):
+    l = []
     i = 0
-    for word in words:
-        if words[i] == words[i+1]:
 
+    while i < len(text):
+        l = text[i].split()
+        l2 = []
+        for number in l:
+            number = int(number)
+            l2.append(symbols[number])
+            text[i] = (" ").join(l2)
+        i += 1
 
+    return text
 
-symbols = {1: "knit", 2: "purl", 3: "ktbl", 4: "ptbl", 5: "bind off", 6: "yo", 
-            7: "M1", 8: "make eyelet", 9: "right-leaning increase", 10: "left-leaning increase", 
-            11: "double increase", 12: "triple increase", 13: "triple purl increase", 14: "increase 1 st", 
-            15: "increase 2 sts", 16: "increase 4 sts", 17: "increase 5 sts", 18: "yo 1 time", 
-            19: "yo 2 times", 20: "yo 3 times", 21: "yo 4 times", 22: "yo 5 times", 23: "ssk", 
-            24: "k2tog", 25: "double decrease", 26: "k3tog", 27: "p2tog tbl", 28: "p2tog", 
-            29: "double purl decrease", 30: "p3tog", 31: "decrease 1 st", 32: "decrease 2 sts", 
-            33: "decrease 3 sts", 34: "decrease 4 sts", 35: "decrease 5 sts", 36: "knit into row below", 
-            37: "purl into row below", 38: "slip st as if to knit", 39: "slip st as if to purl", 
-            40: "slip st with yarn in front", 41: "no stitch", 42: "bobble", 43: "selvage st", 
-            44: "short knit", 45: "short purl"}
+symbols = {1: "knit", 2: "purl", 3: "ktbl", 4: "ptbl", 5: "bind off", 6: "yo", 7: "M1", 8: "make eyelet", 9: "right-leaning increase", 10: "left-leaning increase", 11: "double increase", 12: "triple increase", 13: "triple purl increase", 14: "increase 1 st", 15: "increase 2 sts", 16: "increase 4 sts", 17: "increase 5 sts", 18: "yo 1 time", 19: "yo 2 times", 20: "yo 3 times", 21: "yo 4 times", 22: "yo 5 times", 23: "ssk", 24: "k2tog", 25: "double decrease", 26: "k3tog", 27: "p2tog tbl", 28: "p2tog", 29: "double purl decrease", 30: "p3tog", 31: "decrease 1 st", 32: "decrease 2 sts", 33: "decrease 3 sts", 34: "decrease 4 sts", 35: "decrease 5 sts", 36: "knit into row below", 37: "purl into row below", 38: "slip st as if to knit", 39: "slip st as if to purl", 40: "slip st with yarn in front", 41: "no stitch", 42: "bobble", 43: "selvage st", 44: "short knit", 45: "short purl"}
 
 f = open("static/text/oldshale.txt")
 text = f.readlines()
 f.close()
 
 text.reverse()
-"""If the chart contains patterned WS rows call reverse_even_rows() on the text"""
 
-# instructions = []
-# i = 1
+#If the chart contains patterned WS rows: 
+reverse_even_rows(text)
+
+#Change the number to knitting instructions
+change_to_symbols(text)
+
+#Consoldiate multiple instructions
+
+def transform_multiple(words):
+    i = 0
+
+    for word in words:
+        if words[i] == words[i+1]:
+
 
 # for line in text:
 #     row = line.split()
@@ -54,3 +66,6 @@ text.reverse()
 #         # words.append(symbols[number])
 #         # converted = transform_multiple(words)
 #         instructions.append(symbols[number])
+
+
+ 
