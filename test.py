dae431aa419959a45d70139c5081ff734f6c72e7
Liz Borchardt <zardra@gmail.com>
2013-12-02 16:58:41 -0800

    instructions with row numbers and individual instruction pages

--- test.py
+++ test.py
@@ -91,6 +91,36 @@ def transform_multiples(text):
 
     return text
 
+def create_knit_even_rows(text):
+    """Creates instructions with Row numbers for charts worked evenly on the WS rows"""
+    i = 1
+    instructions = ""
+    knit_even_rows = []
+    
+    for row in text:
+        instructions += "Row %d: %s" % (i, row)
+        knit_even_rows.append(instructions)
+        instructions = "Row %d: Work row evenly" % (i+1)
+        knit_even_rows.append(instructions)
+        instructions = ""
+        i += 2
+    
+    return knit_even_rows
+
+def create_knit_rows(text):
+    """Creates instructions with Row numbers for charts worked evenly on the WS rows"""
+    i = 1
+    instructions = ""
+    knit_rows = []
+
+    for row in text:
+            instructions += "Row %d: %s" % (i, row)
+            i += 1
+            knit_rows.append(instructions)
+            instructions = ""
+    
+    return knit_rows
+
 # dictionary assigning values to knitting instructions
 symbols = {1: "knit", 2: "purl", 3: "ktbl", 4: "ptbl", 5: "bind off", 6: "yo", 7: "M1", 8: "make eyelet", 9: "right-leaning increase", 10: "left-leaning increase", 11: "double increase", 12: "triple increase", 13: "triple purl increase", 14: "increase 1 st", 15: "increase 2 sts", 16: "increase 4 sts", 17: "increase 5 sts", 18: "yo 1 time", 19: "yo 2 times", 20: "yo 3 times", 21: "yo 4 times", 22: "yo 5 times", 23: "ssk", 24: "k2tog", 25: "double decrease", 26: "k3tog", 27: "p2tog tbl", 28: "p2tog", 29: "double purl decrease", 30: "p3tog", 31: "decrease 1 st", 32: "decrease 2 sts", 33: "decrease 3 sts", 34: "decrease 4 sts", 35: "decrease 5 sts", 36: "knit into row below", 37: "purl into row below", 38: "slip st as if to knit", 39: "slip st as if to purl", 40: "slip st with yarn in front", 41: "no stitch", 42: "bobble", 43: "selvage st", 44: "short knit", 45: "short purl"}
 
@@ -105,13 +135,22 @@ def main(button_val):
 
     # if the chart contains patterned WS rows: 
     if button_val == "patterned":
+        # reverse the even numbered rows
         reversed_rows = reverse_even_rows(reversed_text)
+        # change the numbers to the written knitting instructions
         eng_ins = change_to_symbols(reversed_rows)
     else:
-        # change the number to knitting instructions
+        # change the number to written knitting instructions
         eng_ins = change_to_symbols(reversed_text)
 
     # consoldiate multiple instructions
     compressed_ins = transform_multiples(eng_ins)
 
-    return compressed_ins
+    if button_val == "patterned":
+        # add row numbers to the instructions
+        row_directions = create_knit_rows(compressed_ins)
+    else:
+        # add row numbers to the instructions
+        row_directions = create_knit_even_rows(compressed_ins)
+
+    return row_directions
